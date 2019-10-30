c00a1dcee41cc76cacb80d0fbcaf8b7a6899783b
Dakota <dakotalreed@gmail.com>
2019-10-30 11:32:03 +0200

    added projects card

--- src/common.blocks/icon-bar/icon-bar.js
+++ src/common.blocks/icon-bar/icon-bar.js
@@ -1,24 +1,25 @@
 import { breakpoint } from "../base/breakpoint";
 
 for (let iconBar of document.querySelectorAll(".icon-bar")) {
-	window.addEventListener("resize", () => {
-		if (breakpoint.value === "extra-small-breakpoint") {
-			iconBar.querySelector(".icon-bar__content").dataset.open = "false";
-			iconBar.querySelector(".icon-bar__content").style.width = "0rem";
-			iconBar.querySelector(
-				".icon-bar__icon_toggle"
-			).firstElementChild.dataset.icon = "angle-right";
-		} else {
-			iconBar.querySelector(".icon-bar__content").dataset.open = "true";
-			iconBar.querySelector(".icon-bar__content").style.width =
-				"3.125rem";
-			iconBar.querySelector(
-				".icon-bar__icon_toggle"
-			).firstElementChild.dataset.icon = "angle-left";
-		}
-	});
+	// SET ICON-BAR TO OPEN ON SMALL-BREAKPOINT AND ABOVE
+	// window.addEventListener("resize", () => {
+	// 	if (breakpoint.value === "extra-small-breakpoint") {
+	// 		iconBar.querySelector(".icon-bar__content").dataset.open = "false";
+	// 		iconBar.querySelector(".icon-bar__content").style.width = "0rem";
+	// 		iconBar.querySelector(
+	// 			".icon-bar__icon_toggle"
+	// 		).firstElementChild.dataset.icon = "angle-right";
+	// 	} else {
+	// 		iconBar.querySelector(".icon-bar__content").dataset.open = "true";
+	// 		iconBar.querySelector(".icon-bar__content").style.width =
+	// 			"3.125rem";
+	// 		iconBar.querySelector(
+	// 			".icon-bar__icon_toggle"
+	// 		).firstElementChild.dataset.icon = "angle-left";
+	// 	}
+	// });
 
-	window.dispatchEvent(new Event("resize"));
+	// window.dispatchEvent(new Event("resize"));
 
 	iconBar
 		.querySelector(".icon-bar__icon_toggle")
@@ -39,7 +40,7 @@ for (let iconBar of document.querySelectorAll(".icon-bar")) {
 				).firstElementChild.dataset.icon = "angle-right";
 			} else {
 				iconBar.querySelector(".icon-bar__content").style.width =
-					"3.125rem";
+					"100%";
 
 				iconBar.querySelector(".icon-bar__content").dataset.open = true;
 
