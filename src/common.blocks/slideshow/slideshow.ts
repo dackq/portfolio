f992719d4dca4fe5208247ab773c1fadcfff3890
Dakota <dakotalreed@gmail.com>
2019-11-15 11:13:00 +0200

    slideshow styling

--- src/common.blocks/slideshow/slideshow.ts
+++ src/common.blocks/slideshow/slideshow.ts
@@ -4,12 +4,48 @@ class Slideshow extends LitElement {
 	static get properties() {
 		return {};
 	}
+	static get styles() {
+		return css`
+			.carousel {
+				overflow: hidden;
+				width: 90%;
+				box-sizing: border-box;
+			}
+
+			.carousel__track {
+				transform-style: preserve-3d;
+			}
+
+			.carousel__slide {
+				/* opacity: 0; */
+				/* position: absolute; */
+				/* top: 0; */
+				width: 100%;
+				margin: auto;
+				padding: 1rem 4rem;
+				z-index: 100;
+				background: black;
+			}
+		`;
+	}
 	render() {
 		return html`
-			<div>
-				<a>go left</a>
-				<slot></slot>
-				<a>go right</a>
+			<div class="carousel">
+				<button class="carousel__button"></button>
+				<div class="carousel__track">
+					<div class="carousel__slide"></div>
+
+					<!-- <img class="carousel__slide" src="img/slide-1.jpg" alt="" />
+					<img class="carousel__slide" src="img/slide-1.jpg" alt="" />
+					<img class="carousel__slide" src="img/slide-1.jpg" alt="" /> -->
+				</div>
+				<button class="carousel__button"></button>
+
+				<div class="carousel__nav">
+					<button class="carousel__indicator"></button>
+					<button class="carousel__indicator"></button>
+					<button class="carousel__indicator"></button>
+				</div>
 			</div>
 		`;
 	}
