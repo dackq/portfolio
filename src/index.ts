734f337fe912007e224dcb28e130f815d17b5f26
Dakota <dakotalreed@gmail.com>
2019-11-18 11:14:33 +0200

    changed content width to 50rem

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import "@fortawesome/fontawesome-free/js/all";
-import "./styles.scss";
-import "./common.blocks/base/breakpoint";
-import "./common.blocks/icon-bar/icon-bar";
-import "./common.blocks/slideshow/slideshow.js";
