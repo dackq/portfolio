745c9064ee96c3902548412b7a320b7527941402
Dakota <dakotalreed@gmail.com>
2019-11-18 16:33:29 +0200

    changed slideshow design

--- src/common.blocks/slideshow/slideshow.js
+++ src/common.blocks/slideshow/slideshow.js
@@ -2,6 +2,9 @@ import { LitElement, css, html } from "lit-element";
 
 import pomodoro from "../../img/pomodoro.png";
 
+const SlideWidth = 20;
+const SlideHeight = SlideWidth / 1.618;
+
 class Slideshow extends LitElement {
 	static get properties() {
 		return {
@@ -94,6 +97,11 @@ class Slideshow extends LitElement {
 				overflow: hidden;
 				box-sizing: border-box;
 				position: relative;
+				border: 1px solid black;
+				background-color: #333;
+				padding: 1rem 0rem;
+				box-shadow: inset -5px -5px 35px -8px black;
+				border-radius: 1rem;
 			}
 
 			.carousel__track {
@@ -104,20 +112,17 @@ class Slideshow extends LitElement {
 				opacity: 0;
 				position: absolute;
 				display: block;
+				box-shadow: -5px -5px 35px -8px black;
+				background-color: #adadad;
 				top: 0;
 				left: 50%;
 				transform: translateX(-50%);
-				width: 65%;
-				/* border-radius: 0.6rem; */ /* div is taller than image so the bottom berder looks weird. not sure how to make the div the same height as the image. */
+				width: ${SlideWidth}rem;
+				height: ${SlideHeight}rem;
+				border-radius: 0.6rem;
 				z-index: 100;
 				transition: transform 0.5s, opacity 0.5s, z-index 0.5s;
 				overflow: hidden;
-			}
-
-			@media (min-width: 768px) {
-				.carousel__slide {
-					width: 35%;
-				}
 			}
 
 			.carousel__slide_initial,
@@ -126,18 +131,20 @@ class Slideshow extends LitElement {
 				position: relative;
 				margin: 0;
 				z-index: 900;
+				/* transform: translateY(5%); */
 			}
 
 			.carousel__slide_prev,
 			.carousel__slide_next {
+				opacity: 0.2;
 				z-index: 800;
 			}
 
 			.carousel__slide_prev {
-				transform: translateX(-150%);
+				transform: translateX(-120%);
 			}
 			.carousel__slide_next {
-				transform: translateX(50%);
+				transform: translateX(20%);
 			}
 			.carousel__image {
 				width: 100%;
@@ -158,10 +165,19 @@ class Slideshow extends LitElement {
 			}
 
 			.carousel__button_prev {
-				left: 0;
+				left: 2%;
 			}
 			.carousel__button_next {
-				right: 0;
+				right: 2%;
+			}
+
+			@media (min-width: 768px) {
+				.carousel__button_prev {
+					left: 5%;
+				}
+				.carousel__button_next {
+					right: 5%;
+				}
 			}
 			.carousel__button_prev::after,
 			.carousel__button_next::after {
